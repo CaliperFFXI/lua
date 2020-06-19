65a9d3b45735501dad725c11e5db41dcd54b6817
CaliperFFXI <CaliperFFXI@gmail.com>
2020-06-18 22:36:47 -0400

    Delete Copy.bat

--- Copy.bat
+++
@@ -1,3 +0,0 @@
-Xcopy /Y /S /I /E "C:\Users\Cutie Pi\Desktop\Caliper\addons\GearSwap\Data"  "C:\Users\Cutie Pi\Desktop\lua"
-Xcopy /Y /S /I /E "C:\Users\Cutie Pi\Desktop\Caliper\addons\GearSwap\Data\libs"  "C:\Users\Cutie Pi\Desktop\lua\libs"
-RMDIR /S /Q "C:\Users\Cutie Pi\Desktop\lua\export"
