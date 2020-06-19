c0ffe0d92420e0dabc8f3d410a1c9cb7f4761c5b
CaliperFFXI <CaliperFFXI@gmail.com>
2020-06-18 21:58:47 -0400

    mote-mappings update

--- Copy.bat
+++ Copy.bat
@@ -1,2 +1,2 @@
 Xcopy /Y /S /I /E "C:\Users\Cutie Pi\Desktop\Caliper\addons\GearSwap\data"  "C:\Users\Cutie Pi\Desktop\lua"
-
+Xcopy /Y /S /I /E "C:\Users\Cutie Pi\Desktop\Caliper\addons\GearSwap\libs\Mote-Mappings.lua" "C:\Users\Cutie Pi\Desktop\lua\libs"
